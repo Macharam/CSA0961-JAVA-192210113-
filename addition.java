43fd2c1c139e06f9861bc275f11f4a73b031d510
Macharam <128776512+Macharam@users.noreply.github.com>
2024-06-29 08:37:18 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,11 @@
+class addition 
+{
+        public static void main(String arg[])
+        {
+                 int n1,n2,sum;
+                 n1=10;
+                 n2=40;
+                 sum=n1+n2;
+                 System.out.println("Addition:"+sum);
+        }
+}
